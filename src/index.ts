681abef29d2f355d670bee90b22275259efd5dbe
Bucaletti sig. Andrea <ABUCALETTI@tech.ceia>
2018-10-08 10:29:06 +0200

    --

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { BezierSpline, Color } from "./bezier-spline";
-import { Vector } from "./vector";
+import { Color } from "./bezier-spline";
 import { SignCanvas } from "./sign-canvas";
 
 
 window["sc"] = {
-    BezierSpline: BezierSpline,
-    Vector: Vector,
     SignCanvas: SignCanvas,
     Color: Color
 }
